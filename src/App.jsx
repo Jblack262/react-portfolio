db2a67f22b2c09275e62fba04630b943c2bc7d5c
Jblack262 <JBlack262@west-mec.org>
2021-09-27 20:41:35 -0700

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import {links} from './utils/consts';
+import {Home} from './pages'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
